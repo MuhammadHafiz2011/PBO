f887429f7b09a60649f3d1828a54dc7fec6b4fc6
MuhammadHafiz2011 <126840210+MuhammadHafiz2011@users.noreply.github.com>
2023-03-06 20:17:26 +0700

    Add files via upload

---
+++ OutputVariable.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hafiz030323;
+
+/**
+ *
+ * @author MY HP
+ */
+public class OutputVariable
+{
+    public static void main (String[] args)
+    {
+        int value = 10;
+        char x;
+        x = 'A';
+        
+        System.out.println(value);
+        System.out.println("The Value of x=" + x);
+    }
+}
---
+++ VariableSamples.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hafiz030323;
+
+/**
+ *
+ * @author MY HP
+ */
+public class VariableSamples 
+{
+    public static void main( String[] args) {
+      //deklarasi a tipe data dengan nama variabel
+      // result dan tipe data boolean
+      boolean result;
+      //deklarasi tipe data dengan nama variabel
+      //option dan tipe data char
+      char option;
+      option = 'c';  //menandai 'c' sebagai option
+      
+      //deklarasi tipe data dengan nama variabel
+      //grade, double tipe data dan telagh di inisialisasi
+      //to 0.0
+      double grade = 0.0;
+  }     
+}
